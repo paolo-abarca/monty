3904f176cee0e0b85431ca679daabe6e48fb2e83
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-11 18:23:38 -0500

    Fixing header file containing all project prototypes and structures

--- monty.h
+++ monty.h
@@ -1,23 +1,19 @@
-#ifndef __MONTY_H__
-#define __MONTY_H__
+#ifndef MONTY_H
+#define MONTY_H
 
-#include <stdlib.h>
+/* Libraries */
+
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <ctype.h>
-#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-
-#define DELIMS "\n \r\t"
+#include <stdbool.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -27,12 +23,10 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-
 /**
- * struct instruction_s - opcoode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -42,19 +36,70 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t **global_head;
-stack_t **global_head;
+/**
+ * struct settings_s - settings for all the program
+ * @file: file
+ * @line: line
+ * @stack: stack
+ * @queue: false
+ */
+typedef struct settings_s
+{
+	FILE *file;
+	char *line;
+	stack_t *stack;
+	bool queue;
+} settings_t;
 
-void free_glob(void);
-void open_file(char *file, stack_t **stack);
-void get_operator(stack_t **stack, char *op, unsigned int line_num);
+extern settings_t settings;
+settings_t settings;
 
-void _push(stack_t **stack, unsigned int line_number);
-void _pall(stack_t **stack, unsigned int line_number);
-void _pint(stack_t **stack, unsigned int line_number);
-void _pop(stack_t **stack, unsigned int line_number);
-void _swap(stack_t **stack, unsigned int line_number);
-void _add(stack_t **stack, unsigned int line_number);
-void _nop(stack_t **stack, unsigned int line_number);
+
+
+/* helpers */
+void verify_args(int argc);
+void open_and_read(char *f);
+void exec_monty(stack_t **stack, char *opcode, int ln);
+void set(void);
+void clean(void);
+
+/*Tools*/
+int is_number(char *c);
+
+/* Instructions */
+void exec_pall(stack_t **stack, unsigned int line_number);
+void exec_pop(stack_t **stack, unsigned int line_number);
+void exec_pint(stack_t **stack, unsigned int line_number);
+void exec_swap(stack_t **stack, unsigned int line_number);
+void exec_nop(stack_t **stack, unsigned int line_number);
+
+/* String Instructions */
+void exec_pchar(stack_t **stack, unsigned int line_number);
+void exec_pstr(stack_t **stack, unsigned int line_number);
+
+/* Aritmetic Instructions */
+void exec_div(stack_t **stack, unsigned int line_number);
+void exec_mod(stack_t **stack, unsigned int line_number);
+void exec_add(stack_t **stack, unsigned int line_number);
+void exec_sub(stack_t **stack, unsigned int line_number);
+void exec_mul(stack_t **stack, unsigned int line_number);
+
+/* Rotations Instructions*/
+void exec_rotl(stack_t **stack, unsigned int line_number);
+void exec_rotr(stack_t **stack, unsigned int line_number);
+
+/* Errors managements */
+
+void error_handler(char *opcode, int errorcode, int ln);
+
+/* FIFO AND LIFO */
+void exec_stack(stack_t **stack, unsigned int line_number);
+void exec_queue(stack_t **stack, unsigned int line_number);
+
+/* Stack Management */
+
+stack_t *push_stack(stack_t **head, const int n);
+stack_t *push_queue(stack_t **head, const int n);
+void fstack(stack_t *head);
 
 #endif
