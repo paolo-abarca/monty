230ee5c35a8cfe733261eb689e989e9c499f02d8
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-10 22:22:08 -0500

    Adding Second part of the instructions

---
+++ operators_2.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _nop - The opcode nop doesn’t do anything
+ *
+ * @stack: double pointer to the head of the stack
+ * @line_number: is the line number
+ * Return: void
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
+
+/**
+ * _add - The opcode add adds the top two elements of the stack
+ *
+ * @stack: double pointer to the head of the stack
+ * @line_number: is the line number
+ * Return: void
+ */
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int sum;
+
+	if (stack == NULL || (*stack) == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+
+	sum = temp->n + temp->next->n;
+	temp->next->n = sum;
+
+	*stack = temp->next;
+
+	free(temp);
+}
