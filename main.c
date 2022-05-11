ee221af5825a48bf364392909b84773904f789af
JSM788 <salinasjuan788@gmail.com>
2022-05-10 22:15:13 -0500

    First version of our interpreter Monty

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: are the arguments we give to the program
+ * @argv: are the test files
+ * Return: always EXIT_SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	stack_t *head;
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	head = NULL;
+
+	open_file(argv[1], &head);
+
+	exit(EXIT_SUCCESS);
+}
