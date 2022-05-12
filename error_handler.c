15f274e795e2e058502a4249ad2cc1723501f042
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-05-11 23:33:43 -0500

    Updating function that handles errors

--- error_handler.c
+++ error_handler.c
@@ -1,16 +1,19 @@
 #include "monty.h"
+
 /**
- * error_handler - this function executes the div opcode
+ * error_handler - is a function that handles errors
+ *
  * @op: opcode generates error
  * @errorcode: the number of the error
- * @ln: number of line that is executed
- * Return: void function
+ * @ln: is the line number
+ * Return: void
  */
+
 void error_handler(char *op, int errorcode, int ln)
 {
 	if (errorcode == -95)
 	{dprintf(STDERR_FILENO, "L%u: can't %s an empty stack\n", ln, op);
-		clean();
+		free_code();
 		exit(EXIT_FAILURE);
 	}
 	if (errorcode == -96)
@@ -21,31 +24,31 @@ void error_handler(char *op, int errorcode, int ln)
 	if (errorcode == -97)
 	{
 		dprintf(STDERR_FILENO, "Error: %s failed\n", op);
-		clean();
+		free_code();
 		exit(EXIT_FAILURE);
 	}
 	if (errorcode == -98)
 	{
 		dprintf(STDERR_FILENO, "L%u: can't %s, stack empty\n", ln, op);
-		clean();
+		free_code();
 		exit(EXIT_FAILURE);
 	}
 	if (errorcode == -99)
 		dprintf(STDERR_FILENO,
-		"L%u: can't %s, stack too short\n", ln, op), clean(), exit(EXIT_FAILURE);
+		"L%u: can't %s, stack too short\n", ln, op), free_code(), exit(EXIT_FAILURE);
 	if (errorcode == -126)
 		dprintf(STDERR_FILENO,
-		"L%u: division by zero\n", ln), clean(), exit(EXIT_FAILURE);
+		"L%u: division by zero\n", ln), free_code(), exit(EXIT_FAILURE);
 	if (errorcode == -127)
 		dprintf(STDERR_FILENO,
-		"L%u: can't %s, value out of range\n", ln, op), clean(), exit(EXIT_FAILURE);
+		"L%u: can't %s, value out of range\n", ln, op),
+free_code(), exit(EXIT_FAILURE);
 	if (errorcode == -128)
 		dprintf(STDERR_FILENO,
-		"L%d: unknown instruction %s\n", ln, op), clean(), exit(EXIT_FAILURE);
+		"L%d: unknown instruction %s\n", ln, op), free_code(), exit(EXIT_FAILURE);
 	if (errorcode == -129)
 	{
 		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", ln);
-		clean();
-		exit(EXIT_FAILURE);
-	}
+		free_code();
+		exit(EXIT_FAILURE); }
 }
